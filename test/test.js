e47d15e5b96a02fbd3146b92bef5239a374b1e3a
Ivan Demidov <scrum@list.ru>
2018-02-26 16:35:38 +0300

    test: for map get

---
+++ test/test.js
@@ -0,0 +1,20 @@
+import postcss from 'postcss';
+import test from 'ava';
+import plugin from '../src';
+
+const processing = (input, opts) => {
+    return postcss([plugin(opts)]).process(input).css;
+};
+
+test('it should return body color for background', t => {
+    const expected = 'body{background: #fff;}';
+    const value = 'body{background: map-get((body: #fff,main-red: #c53831,link-blue: #0592fb) !default, body);}';
+    t.is(processing(value), expected);
+});
+
+test('it should return body color and min-width width fro decl', t => {
+    const expected = 'body{background: #fff;min-width: 1280px;}';
+    const value = 'body{background: map-get((body: #fff,main-red: #c53831,link-blue: #0592fb) !default, body);min-width: map-get((xxs: 0,xs: 576px,sm: 768px,md: 992px,lg: 1280px,xl: 1360px,xxl: 1600px) !default, lg);}';
+    t.is(processing(value), expected);
+});
+
