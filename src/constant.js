c75b461a228c4e7906977e5ae7150166118eea4f
Marco Pasqualetti <marco.pasqualetti@live.com>
2019-11-24 08:20:28 +0100

    fix: add error for not closed parenthesis in parse-parenthesis-content

--- src/constant.js
+++ src/constant.js
@@ -1,1 +1,3 @@
 export const METHOD = 'map-get((';
+
+export const ERROR_PREFIX = 'postcss – map-get';
--- src/parse-parenthesis-content.js
+++ src/parse-parenthesis-content.js
@@ -1,7 +1,9 @@
+import {ERROR_PREFIX} from './constant';
+
 /**
  * Get all the content inside brackets
- * @param {string} stringToParse string with parenthesis. This string must start with an `()`
- * @param {string} [startingPosition] to start parsing
+ * @param {string} stringToParse string with parenthesis. This string must start with an `(`
+ * @param {number} [startingPosition] to start parsing
  * @returns {{content: string, position: number}} All the content inside parenthesis including nested properties
  *
  * @todo might worth to parse the string until a `(` is found?
@@ -11,10 +13,10 @@ export default function parseParenthesisContent(stringToParse, startingPosition 
   let content = '';
 
   const stack = [];
-  while (true) { // eslint-disable-line no-constant-condition
+  while (position !== stringToParse.length) {
     const mapChracter = stringToParse[position];
     if (mapChracter === '(') {
-      stack.push(mapChracter);
+      stack.push(position);
     } else if (mapChracter === ')') {
       stack.pop();
     }
@@ -25,6 +27,13 @@ export default function parseParenthesisContent(stringToParse, startingPosition 
     if (stack.length === 0) {
       break;
     }
+  }
+
+  /**
+   * String should be already validated by postcss but to avoid unclear stacktrace I'll manage the error anyway.
+   */
+  if (stack.length !== 0) {
+    throw new Error(`${ERROR_PREFIX} – parenthesis not closed`);
   }
 
   return {content, position};
